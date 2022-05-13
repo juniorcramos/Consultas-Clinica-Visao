2573248f6c75a686c7538d1575ad0dff0c7a0ff1
Junior <junior.cassiano.ramos@hotmail.com>
2022-05-13 19:08:01 -0300

    Primeira tela pronta, iniciada a tela de agendar as consultas

---
+++ app/src/main/java/junior/ramos/consultas/AgendarConsulta.java
@@ -0,0 +1,18 @@
+package junior.ramos.consultas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AgendarConsulta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agendar_consulta);
+    }
+}
--- app/src/main/java/junior/ramos/consultas/MainActivity.java
+++ app/src/main/java/junior/ramos/consultas/MainActivity.java
@@ -1,5 +1,6 @@
 package junior.ramos.consultas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,11 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //criando a iteração de clicar no botão e ir para a proxima tela
         agendarConsulta = findViewById(R.id.buttonAdicionarConsulta);
         agendarConsulta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(getApplicationContext(),AgendarConsulta.class);
+                startActivity(intent);
             }
         });
     }
