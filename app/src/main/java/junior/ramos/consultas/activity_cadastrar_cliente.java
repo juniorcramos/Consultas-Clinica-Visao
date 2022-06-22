296a70df710358dd1cc7beb4b14cf7e753876ae9
Júnior Ramos <junior.cassiano.ramos@hotmail.com>
2022-06-22 16:36:27 -0300

    Criação do método Salvar.

--- app/src/main/java/junior/ramos/consultas/activity_cadastrar_cliente.java
+++ app/src/main/java/junior/ramos/consultas/activity_cadastrar_cliente.java
@@ -3,8 +3,11 @@ package junior.ramos.consultas;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+
+import java.util.Objects;
 
 public class activity_cadastrar_cliente extends Activity {
 
@@ -24,5 +27,22 @@ public class activity_cadastrar_cliente extends Activity {
         spTipos = (Spinner) findViewById(R.id.spinnerTiposPet);
         ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.selecionarTipoAnimal, android.R.layout.simple_list_item_1);//Passa o array para o speaner
         spTipos.setAdapter(adapter);
+    }
+
+    public void salvar(View view){
+        TextInputEditText entradaNomeCliente = findViewById(R.id.entradaNomeCliente); // captura o valor digitado na entrada de nome do cliente
+        String nomeUsuario = Objects.requireNonNull(entradaNomeCliente.getText()).toString();// pega o valor digitado que é editable e transforma em um objeto do tipo String
+
+        TextInputEditText entradaEnderecoCliente = findViewById(R.id.entradaEnderecoCliente);
+        String enderecoCliente = entradaEnderecoCliente.getText().toString();
+
+        TextInputEditText entradaTelefoneCliente = findViewById(R.id.entradaTelefoneCliente);
+        int telefoneCliente = Integer.parseInt(entradaTelefoneCliente.getText().toString());
+
+        TextInputEditText entradaNomePet = findViewById(R.id.entradaNomePet);
+        String nomePet = Objects.requireNonNull(entradaNomePet.getText()).toString();
+
+        TextInputEditText entradaIdadePet = findViewById(R.id.entradaIdadePet);
+        int idadePet = Integer.parseInt(entradaIdadePet.getText().toString());
     }
 }
